733d6ecb60d9747503d225a5356babd6b3b7efaa
Sandro Torres <sandrotorres.ti@gmail.com>
2018-05-15 08:13:26 -0300

    Voltando ao estado anterior

--- node/temporizador.js
+++ node/temporizador.js
@@ -1,11 +1,11 @@
 const schedule = require('node-schedule')
 
-const tarefa0 = schedule.scheduleJob('*/5 * 18 * * 1', function () {
+const tarefa1 = schedule.scheduleJob('*/5 * 18 * * 1', function () {
     console.log('Executando Tarefa 1!', new Date().getSeconds())
 })
 
 setTimeout(function () {
-    tarefa0.cancel()
+    tarefa1.cancel()
     console.log('Cancelamento Tarefa 1!')
 }, 20000)
 
@@ -14,6 +14,6 @@ regra.dayOfWeek = [new schedule.Range(1, 5)]
 regra.hour = 18
 regra.second = 30
 
-const tarefa1 = schedule.scheduleJob(regra, function () {
+const tarefa2 = schedule.scheduleJob(regra, function () {
     console.log('Executando Tarefa 2!', new Date().getSeconds())
 })
