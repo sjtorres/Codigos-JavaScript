2be855ac3c2fa87cb010758ec02b4e78393361d0
Sandro Torres <sandrotorres.ti@gmail.com>
2018-05-20 07:41:32 -0300

    Add Revisão 01

--- esNext/revisao1.js
+++ esNext/revisao1.js
@@ -6,5 +6,18 @@
 }
 
 console.log(a)
+// console.log(b)
 
-//
+// Template String
+const produto = 'iPad'
+console.log(`O ${produto} é caro!`)
+
+//Destructuring
+const [l, e, ...tras] = "Coder"
+console.log(l, e, tras)
+
+const [x, y] = [1, 2, 3]
+console.log(x, y)
+
+const { idade: i, nome } = { nome: 'Sandro', idade: 44 }
+console.log(i, nome)
